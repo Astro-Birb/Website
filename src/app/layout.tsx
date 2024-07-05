a1ca611f58482efc46ff8e6a67171128312ba10c
Bugs <deezbird2768@gmail.com>
2024-07-05 21:16:31 +0100

    Can't be asked to wait & idc about a 404 page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,43 +1,16 @@
-"use client";
-import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NotFound from '../pages/notfound';
-import Home from './page';
+import React, {} from 'react';
 
-const DynamicPage = ({ page }: { page: string }) => {
-  const PageComponent = lazy(() =>
-      import(`@/pages/${page}`).catch(() => ({
-        default: NotFound,
-      }))
-  );
-// unlikely it'll ever need to show loading but better to have it
-  return (
-      <Suspense fallback={<div>Loading...</div>}>
-        <PageComponent />
-      </Suspense>
-  );
-};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <Router>
-      <html lang="en">
-        <body>
-        <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route
-            path="/:page"
-            element={<DynamicPage page={window.location.pathname.slice(1)} />}
-          />
-          <Route path="*" element={<NotFound/>} />
-        </Routes>
-        {children}
-        </body>
-      </html>
-    </Router>
-  );
+    <html lang="en">
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  )
 }
