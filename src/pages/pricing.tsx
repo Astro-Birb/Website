ce262b02cdfdfa6d4b061d793db50584cfdae5c6
Bugs <deezbird2768@gmail.com>
2024-07-06 13:18:27 +0100

    Pricing
    <3 flowbite

---
+++ src/pages/pricing.tsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import '../app/globals.css';
+import Footer from '../components/footer';
+import Header from "../components/header";
+
+export default function Premium() {
+    return (
+        <div className="flex flex-col min-h-screen bg-gradient-to-b from-zinc-800 to-zinc-950 text-white font-sans">
+            <Header />
+            <main className="flex-grow flex items-center justify-center px-4 mt-15 pt-[100px]">
+                <div className="flex space-x-5">
+                    <div className="flex-1 flex flex-col max-w-[400px] p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-zinc-900 dark:border-zinc-700">
+                        <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">Premium</h5>
+                        <div className="flex items-baseline text-gray-900 dark:text-white">
+                            <span className="text-3xl font-semibold">$</span>
+                            <span className="text-5xl font-extrabold tracking-tight">3</span>
+                            <span className="ms-1 text-xl font-normal text-gray-500 dark:text-gray-400">/month</span>
+                        </div>
+                        <ul role="list" className="flex-1 flex flex-col justify-center space-y-5 my-7">
+                            <li className="flex items-center">
+                                <svg className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+                                </svg>
+                                <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">More Custom Commands</span>
+                            </li>
+                            <li className="flex">
+                                <svg className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+                                </svg>
+                                <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Mass Infract</span>
+                            </li>
+                            <li className="flex">
+                                <svg className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+                                </svg>
+                                <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">More Applications</span>
+                            </li>
+                            <li className="flex">
+                                <svg className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+                                </svg>
+                                <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Premium Hangout</span>
+                            </li>
+                            <li className="flex">
+                                <svg className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+                                </svg>
+                                <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Exclusive Modules</span>
+                            </li>
+                        </ul>
+                        <a href="https://www.patreon.com/checkout/AstroBirb?rid=22855340" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">
+                            Choose plan
+                        </a>
+                    </div>
+                    
+                    <div className="flex-1 flex flex-col max-w-[300px] p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-zinc-900 dark:border-zinc-700">
+                        <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">Custom Branding</h5>
+                        <div className="flex items-baseline text-gray-900 dark:text-white">
+                            <span className="text-3xl font-semibold">$</span>
+                            <span className="text-5xl font-extrabold tracking-tight">5</span>
+                            <span className="ms-1 text-xl font-normal text-gray-500 dark:text-gray-400">/month</span>
+                        </div>
+                        <ul role="list" className="flex-1 flex flex-col justify-center space-y-5 my-7">
+                            <li className="flex items-center">
+                                <svg className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+                                </svg>
+                                <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Premium</span>
+                            </li>
+                            <li className="flex">
+                                <svg className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+                                </svg>
+                                <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Custom Bot</span>
+                            </li>
+                            <li className="flex">
+                                <svg className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+                                </svg>
+                                <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Custom Bot Status</span>
+                            </li>
+                            <li className="flex">
+                                <svg className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+                                </svg>
+                                <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Custom Branding Hangout</span>
+                            </li>
+                            <li className="flex">
+                                <svg className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+                                </svg>
+                                <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Prioritized Support</span>
+                            </li>
+                        </ul>
+                        <a href="https://www.patreon.com/checkout/AstroBirb?rid=22733636" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">
+                            Choose plan
+                        </a>
+                    </div>
+                </div>
+
+            </main>
+            <Footer />
+        </div>
+    );
+}
