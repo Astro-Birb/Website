a6500912ab479f9263f6d0532b6a8304f80bb4d5
Bugs <deezbird2768@gmail.com>
2024-07-06 13:59:56 +0100

    (LOGIN) prolly for applications or ban appeals web

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-
+import {
+    ClerkProvider,
+    SignInButton,
+    SignedIn,
+    SignedOut,
+    UserButton
+  } from '@clerk/nextjs'
 export default function Header() {
     return (
         <header
@@ -13,7 +19,21 @@ export default function Header() {
                 />
                 Astro Birb
             </a>
-        </nav>
-    </header>
-    )
+            <div className="flex items-center">
+                    <ClerkProvider>
+                        <SignedIn>
+                            <UserButton />
+                        </SignedIn>
+                        <SignedOut>
+                            <div className="relative">
+                                <SignInButton>
+                                <button type="button" className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-indigo-600 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Login With Discord </button>
+                                </SignInButton>
+                            </div>
+                        </SignedOut>
+                    </ClerkProvider>
+                </div>
+            </nav>
+        </header>
+    );
 }
