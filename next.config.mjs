b43f0ae7a08b2e0f08cee8a8b18b0cca0ffd76f8
Bugs <deezbird2768@gmail.com>
2024-07-07 16:04:08 +0100

    Adding redirects ffor support, apply, docs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+          {
+            source: '/support', 
+            destination: 'https://discord.gg/DhWdgfh3hN',
+            permanent: true, 
+          },
+          {
+            source: '/apply', 
+            destination: 'https://docs.google.com/forms/d/1m2-wmbA1Ctmk-YCi8zQDjZucHX5z18KaVfz0dmBBDCo/viewform',
+            permanent: true, 
+          },
+          {
+            source: '/docs',
+            destination: 'https://docs.astrobirb.dev',
+            permanent: true,
+            
+          }
+        ];
+      },
+
+};
+
+
 
 export default nextConfig;
