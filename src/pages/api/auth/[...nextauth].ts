7ded9aad3fa548702c06ad178bf41651c9f6987d
Bugs <deezbird2768@gmail.com>
2024-07-09 18:20:11 +0100

    ok

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -7,12 +7,67 @@ const clientSecret = process.env.DISCORD_CLIENT_SECRET;
 if (!clientId || !clientSecret) {
   throw new Error('Missing environment variables DISCORD_CLIENT_ID or DISCORD_CLIENT_SECRET');
 }
-export default NextAuth({
+export const authOptions = {
+  
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     DiscordProvider({
-      clientId,
-      clientSecret,
-      authorization: { params: { scope: 'identify guilds' } }
-    })
+      // @ts-ignore
+      clientId: process.env.DISCORD_CLIENT_ID,
+      // @ts-ignore
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: "identify guilds",
+        },
+      },
+      profile(profile) {
+        console.log(profile);
+        if (profile.avatar === null) {
+          const defaultAvatarNumber = parseInt(profile.discriminator) % 5;
+          profile.image_url = `https://cdn.discordapp.com/embed/avatars/${defaultAvatarNumber}.png`;
+        } else {
+          const format = profile.avatar.startsWith("a_") ? "gif" : "png";
+          profile.image_url = `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.${format}`;
+        }
+
+        return {
+          id: profile.id,
+          name: profile.username,
+          discriminator: profile.discriminator,
+          image: profile.image_url,
+          accentColor: profile.accentColor,
+        };
+      },
+    }),
   ],
-})
+  callbacks: {
+    //@ts-ignore
+    jwt: async ({ token, account, profile }) => {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.tokenType = account.token_type;
+      }
+      if (profile) {
+        token.profile = profile;
+      }
+      return token;
+    },
+    // @ts-ignore
+    session: async ({ session, token }) => {
+      // @ts-ignore
+      session.accessToken = token.accessToken;
+      // @ts-ignore
+      session.refreshToken = token.refreshToken;
+      // @ts-ignore
+      session.tokenType = token.tokenType;
+      // @ts-ignore
+      session.discordUser = token.profile;
+      // @ts-ignore
+      
+      return session;
+    },
+  },
+};
+// @ts-ignore
+export default NextAuth(authOptions)
