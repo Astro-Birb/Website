c1b44fef22ddd3c64792b5a1eae6ed6302eec752
Bugs <deezbird2768@gmail.com>
2024-07-11 21:00:12 +0100

    ok

--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -7,79 +7,79 @@ import { SessionProvider } from "next-auth/react";
 export default function PrivacyPolicy() {
     return (
         <div className="flex flex-col min-h-screen bg-gradient-to-b from-zinc-800 to-zinc-950 text-white font-sans">
-          <SessionProvider>                
-            <Header />
-            <main className="flex-grow flex flex-col items-center justify-center px-4 mt-15 pt-[100px]">
-                <section className="text-left max-w-4xl mx-auto pt-14">
-                    <h2 className="text-5xl font-bold mb-8">
-                        Privacy Policy
-                    </h2>
-                    <article className="mb-8">
-                        <h3 className="text-2xl font-bold mb-2">1. Introduction</h3>
-                        <p className="text-lg text-gray-300">
-                            Welcome to Astro Birb! We are committed to protecting your privacy. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you use our services. By accessing or using Astro Birb, you agree to the terms of this policy. If you have any questions or concerns, please contact us.
-                        </p>
-                    </article>
-                    <article className="mb-8">
-                        <h3 className="text-2xl font-bold mb-2">2. Information We Collect</h3>
-                        <p className="text-lg text-gray-300">
-                            We collect different types of information to provide and improve our service:
-                        </p>
-                        <ul className="list-disc ml-5 text-lg text-gray-300">
-                            <li>
-                                <strong>Public User Information:</strong> Usernames, discriminators, IDs, and avatars, which help us identify and communicate with you.
-                            </li>
-                            <li>
-                                <strong>Public Guild Information:</strong> Server names, IDs, icons, members, channels, and roles, which help us support server-specific functionalities.
-                            </li>
-                        </ul>
-                    </article>
-                    <article className="mb-8">
-                        <h3 className="text-2xl font-bold mb-2">3. How We Use Your Information</h3>
-                        <p className="text-lg text-gray-300">
-                            We use your information to:
-                        </p>
-                        <ul className="list-disc ml-5 text-lg text-gray-300">
-                            <li>Provide and maintain our services.</li>
-                            <li>Improve your experience on Astro Birb.</li>
-                            <li>Ensure the security of our platform and prevent misuse.</li>
-                            <li>Analyze usage to understand and enhance our service offerings.</li>
-                        </ul>
-                    </article>
-                    <article className="mb-8">
-                        <h3 className="text-2xl font-bold mb-2">4. Data Security</h3>
-                        <p className="text-lg text-gray-300">
-                            Your data security is important to us. We implement industry-standard security measures to protect your information from unauthorized access, disclosure, or misuse. We regularly review our practices to enhance the safety of your data.
-                        </p>
-                    </article>
-                    <article className="mb-8">
-                        <h3 className="text-2xl font-bold mb-2">5. Third-Party Services</h3>
-                        <p className="text-lg text-gray-300">
-                            We use <strong>MongoDB Atlas</strong> to securely store your data. MongoDB Atlas is a trusted third-party service known for its robust security measures, ensuring your information is safe and compliant with industry standards and regulations.
-                        </p>
-                    </article>
-                    <article className="mb-8">
-                        <h3 className="text-2xl font-bold mb-2">6. Children's Privacy</h3>
-                        <p className="text-lg text-gray-300">
-                            Astro Birb is not intended for children under the age of 13. We do not knowingly collect personal information from users under 13. If you believe we have inadvertently collected such data, please reach out to us immediately, and we will take steps to delete the information.
-                        </p>
-                    </article>
-                    <article className="mb-8">
-                        <h3 className="text-2xl font-bold mb-2">7. Changes to This Privacy Policy</h3>
-                        <p className="text-lg text-gray-300">
-                            We may update this Privacy Policy from time to time to reflect changes in our practices or for legal, technical, or regulatory reasons. We will notify you of any significant changes by posting the new policy on our website. Your continued use of our services following these changes means you accept the revised policy.
-                        </p>
-                    </article>
-                    <article className="mb-8">
-                        <h3 className="text-2xl font-bold mb-2">8. Contact Us</h3>
-                        <p className="text-lg text-gray-300">
-                            If you have any questions or concerns about this Privacy Policy, please contact us through Astro Birb's Support Server or email us at <a href="mailto:help@astrobirb.com" className="text-blue-400">help@astrobirb.com</a>.
-                        </p>
-                    </article>
-                </section>
-            </main>
-            <Footer />
-            </SessionProvider>                  
+            <SessionProvider>
+                <Header />
+                <main className="flex-grow flex flex-col items-center justify-center px-4 mt-15 pt-[100px]">
+                    <section className="text-left max-w-4xl mx-auto pt-14">
+                        <h2 className="text-5xl font-bold mb-8">
+                            Privacy Policy
+                        </h2>
+                        <article className="mb-8">
+                            <h3 className="text-2xl font-bold mb-2">1. Introduction</h3>
+                            <p className="text-lg text-gray-300">
+                                Welcome to Astro Birb! We are committed to protecting your privacy. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you use our services. By accessing or using Astro Birb, you agree to the terms of this policy. If you have any questions or concerns, please contact us.
+                            </p>
+                        </article>
+                        <article className="mb-8">
+                            <h3 className="text-2xl font-bold mb-2">2. Information We Collect</h3>
+                            <p className="text-lg text-gray-300">
+                                We collect different types of information to provide and improve our service:
+                            </p>
+                            <ul className="list-disc ml-5 text-lg text-gray-300">
+                                <li>
+                                    <strong>Public User Information:</strong> Usernames, discriminators, IDs, and avatars, which help us identify and communicate with you.
+                                </li>
+                                <li>
+                                    <strong>Public Guild Information:</strong> Server names, IDs, icons, members, channels, and roles, which help us support server-specific functionalities.
+                                </li>
+                            </ul>
+                        </article>
+                        <article className="mb-8">
+                            <h3 className="text-2xl font-bold mb-2">3. How We Use Your Information</h3>
+                            <p className="text-lg text-gray-300">
+                                We use your information to:
+                            </p>
+                            <ul className="list-disc ml-5 text-lg text-gray-300">
+                                <li>Provide and maintain our services.</li>
+                                <li>Improve your experience on Astro Birb.</li>
+                                <li>Ensure the security of our platform and prevent misuse.</li>
+                                <li>Analyze usage to understand and enhance our service offerings.</li>
+                            </ul>
+                        </article>
+                        <article className="mb-8">
+                            <h3 className="text-2xl font-bold mb-2">4. Data Security</h3>
+                            <p className="text-lg text-gray-300">
+                                Your data security is important to us. We implement industry-standard security measures to protect your information from unauthorized access, disclosure, or misuse. We regularly review our practices to enhance the safety of your data.
+                            </p>
+                        </article>
+                        <article className="mb-8">
+                            <h3 className="text-2xl font-bold mb-2">5. Third-Party Services</h3>
+                            <p className="text-lg text-gray-300">
+                                We use <strong>MongoDB Atlas</strong> to securely store your data. MongoDB Atlas is a trusted third-party service known for its robust security measures, ensuring your information is safe and compliant with industry standards and regulations.
+                            </p>
+                        </article>
+                        <article className="mb-8">
+                            <h3 className="text-2xl font-bold mb-2">6. Children's Privacy</h3>
+                            <p className="text-lg text-gray-300">
+                                Astro Birb is not intended for children under the age of 13. We do not knowingly collect personal information from users under 13. If you believe we have inadvertently collected such data, please reach out to us immediately, and we will take steps to delete the information.
+                            </p>
+                        </article>
+                        <article className="mb-8">
+                            <h3 className="text-2xl font-bold mb-2">7. Changes to This Privacy Policy</h3>
+                            <p className="text-lg text-gray-300">
+                                We may update this Privacy Policy from time to time to reflect changes in our practices or for legal, technical, or regulatory reasons. We will notify you of any significant changes by posting the new policy on our website. Your continued use of our services following these changes means you accept the revised policy.
+                            </p>
+                        </article>
+                        <article className="mb-8">
+                            <h3 className="text-2xl font-bold mb-2">8. Contact Us</h3>
+                            <p className="text-lg text-gray-300">
+                                If you have any questions or concerns about this Privacy Policy, please contact us through Astro Birb's Support Server or email us at <a href="mailto:help@astrobirb.com" className="text-blue-400">help@astrobirb.com</a>.
+                            </p>
+                        </article>
+                    </section>
+                </main>
+                <Footer />
+            </SessionProvider>
         </div>
     );
 }
--- src/pages/terms.tsx
+++ src/pages/terms.tsx
@@ -7,62 +7,62 @@ import { SessionProvider } from "next-auth/react";
 export default function TermsOfService() {
     return (
         <div className="flex flex-col min-h-screen bg-gradient-to-b from-zinc-800 to-zinc-950 text-white font-sans">
-          <SessionProvider>         
-            <Header />    
-            <main className="flex-grow flex flex-col items-center justify-center px-4 mt-15 pt-[100px]">
-                <section className="text-left max-w-4xl mx-auto pt-14">
-                    <h2 className="text-5xl font-bold mb-8">
-                        Terms of Service
-                    </h2>
-                    <p className="text-lg text-gray-300 mb-8">
-                        By interacting with Astro Birb on Discord, you agree to abide by the following rules when using our service:
-                    </p>
-                    
-                    <article className="mb-8">
-                        <h3 className="text-2xl font-bold mb-2">1. Eligibility</h3>
-                        <p className="text-lg text-gray-300 mb-4">
-                            Individuals under the age of 13 are not eligible to use our services.
+            <SessionProvider>
+                <Header />
+                <main className="flex-grow flex flex-col items-center justify-center px-4 mt-15 pt-[100px]">
+                    <section className="text-left max-w-4xl mx-auto pt-14">
+                        <h2 className="text-5xl font-bold mb-8">
+                            Terms of Service
+                        </h2>
+                        <p className="text-lg text-gray-300 mb-8">
+                            By interacting with Astro Birb on Discord, you agree to abide by the following rules when using our service:
                         </p>
-                    </article>
-                    
-                    <article className="mb-8">
-                        <h3 className="text-2xl font-bold mb-2">2. Compliance with Discord's Terms of Service</h3>
-                        <p className="text-lg text-gray-300 mb-4">
-                            You must adhere to Discord's Terms of Service while using Astro Birb.
-                        </p>
-                    </article>
-                    
-                    <article className="mb-8">
-                        <h3 className="text-2xl font-bold mb-2">3. Prohibited Activities</h3>
-                        <p className="text-lg text-gray-300 mb-4">
-                            You are prohibited from impersonating any staff member of Astro Birb or the bot itself. Spamming commands or engaging in activities that negatively impact the bot's performance is strictly prohibited.
-                        </p>
-                    </article>
-                    
-                    <article className="mb-8">
-                        <h3 className="text-2xl font-bold mb-2">4. User Information</h3>
-                        <p className="text-lg text-gray-300 mb-4">
-                            By using our service, you acknowledge and agree that we reserve the right to collect basic user information in accordance with our Privacy Policy.
-                        </p>
-                    </article>
-                    
-                    <article className="mb-8">
-                        <h3 className="text-2xl font-bold mb-2">5. Changes to Terms of Service</h3>
-                        <p className="text-lg text-gray-300 mb-4">
-                            We may update these Terms of Service from time to time. We will notify you of any changes by posting the updated Terms of Service on our website or through other appropriate communication channels.
-                        </p>
-                    </article>
-                    
-                    <article className="mb-8">
-                        <h3 className="text-2xl font-bold mb-2">6. Contact Us</h3>
-                        <p className="text-lg text-gray-300">
-                            If you have any questions or concerns about these Terms of Service, please contact us through Astro Birb's Support Server or by emailing us at <a href="mailto:support@astrobirb.com" className="text-blue-400">support@astrobirb.com</a>.
-                        </p>
-                    </article>
-                </section>
-            </main>
-            <Footer />
-            </SessionProvider>               
+
+                        <article className="mb-8">
+                            <h3 className="text-2xl font-bold mb-2">1. Eligibility</h3>
+                            <p className="text-lg text-gray-300 mb-4">
+                                Individuals under the age of 13 are not eligible to use our services.
+                            </p>
+                        </article>
+
+                        <article className="mb-8">
+                            <h3 className="text-2xl font-bold mb-2">2. Compliance with Discord's Terms of Service</h3>
+                            <p className="text-lg text-gray-300 mb-4">
+                                You must adhere to Discord's Terms of Service while using Astro Birb.
+                            </p>
+                        </article>
+
+                        <article className="mb-8">
+                            <h3 className="text-2xl font-bold mb-2">3. Prohibited Activities</h3>
+                            <p className="text-lg text-gray-300 mb-4">
+                                You are prohibited from impersonating any staff member of Astro Birb or the bot itself. Spamming commands or engaging in activities that negatively impact the bot's performance is strictly prohibited.
+                            </p>
+                        </article>
+
+                        <article className="mb-8">
+                            <h3 className="text-2xl font-bold mb-2">4. User Information</h3>
+                            <p className="text-lg text-gray-300 mb-4">
+                                By using our service, you acknowledge and agree that we reserve the right to collect basic user information in accordance with our Privacy Policy.
+                            </p>
+                        </article>
+
+                        <article className="mb-8">
+                            <h3 className="text-2xl font-bold mb-2">5. Changes to Terms of Service</h3>
+                            <p className="text-lg text-gray-300 mb-4">
+                                We may update these Terms of Service from time to time. We will notify you of any changes by posting the updated Terms of Service on our website or through other appropriate communication channels.
+                            </p>
+                        </article>
+
+                        <article className="mb-8">
+                            <h3 className="text-2xl font-bold mb-2">6. Contact Us</h3>
+                            <p className="text-lg text-gray-300">
+                                If you have any questions or concerns about these Terms of Service, please contact us through Astro Birb's Support Server or by emailing us at <a href="mailto:support@astrobirb.com" className="text-blue-400">support@astrobirb.com</a>.
+                            </p>
+                        </article>
+                    </section>
+                </main>
+                <Footer />
+            </SessionProvider>
         </div>
     );
 }
