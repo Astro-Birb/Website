a8f7614358c5b7b7426832f16a9f598c5f03af99
Bugs <deezbird2768@gmail.com>
2024-07-14 01:49:24 +0100

    Update [guildid].tsx

--- src/pages/dashboard/[guildid].tsx
+++ src/pages/dashboard/[guildid].tsx
@@ -99,7 +99,7 @@ const GuildDashboard = () => {
                     <input
                       type="text"
                       id="prefix-input"
-                      value={prefix ?? ''}
+                      defaultValue={prefix ?? ''}
                       className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     />
                   </div>
