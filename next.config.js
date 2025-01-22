9a234a729b2177dafba329b5d7de9b5273c07233
Bugsy <124417333+bugsbirb@users.noreply.github.com>
2025-01-22 22:02:02 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/support', 
+        destination: 'https://discord.gg/DhWdgfh3hN',
+        permanent: true, 
+      },
+      {
+        source: '/apply', 
+        destination: 'https://docs.google.com/forms/d/1m2-wmbA1Ctmk-YCi8zQDjZucHX5z18KaVfz0dmBBDCo/viewform',
+        permanent: true, 
+      },
+      {
+        source: '/docs',
+        destination: 'https://docs.astrobirb.dev',
+        permanent: true,
+        
+      },
+    {
+      source: '/status',
+      destination: 'https://status.astrobirb.dev',
+      permanent: true,
+    }
+    ];
+  },
   output: 'standalone',
   images: {
     domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com', 'cdn.discordapp.com']
 
   }
+  
 }
+
 
 module.exports = nextConfig
